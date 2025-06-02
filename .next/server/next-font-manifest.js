4473267bd9fb74804a3d687768cc5b5cd277810c
jyotsanh <hamaljyotsan@gmail.com>
2025-06-02 09:15:44 +0545

    .next deployed

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/b0g0/Downloads/temp/jyotsanh/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
